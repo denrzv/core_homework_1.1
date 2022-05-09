f084721a61646a9f6443ed2522ff5ccbdac8918f
denrzv <voofaa@gmail.com>
2022-05-09 15:19:18 +0300

    fixed divide method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.divide.apply(a, b);
+        calc.println.accept(c);
     }
 }
